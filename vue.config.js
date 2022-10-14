bfdf0d52d63eb9c3e6ec4c66263ec2dc5e4c8ca9
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-14 13:10:39 +0800

    deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/pet-demo/'
+    : '/',
   transpileDependencies: [
       'vue-meta',
   ],
