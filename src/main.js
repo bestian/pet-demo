e4b3a22acec095849368b79edfb5c1c3c9e4dcdf
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-29 17:39:22 +0800

    s

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import { createMetaManager } from 'vue-meta'
 import { plugin as vueMetaPlugin } from 'vue-meta'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App)
+.use(VueAxios, axios)
 .use(router)
 .use(vueMetaPlugin)
 .use(createMetaManager())
